eaea9256617978b5224310e8831d467df82b1e73
Sameer Vohra <vohra.sam@gmail.com>
2019-07-05 17:20:06 -0400

    WIP - Add terminating condition to http retryer for context.DeadlineExceeded

--- atc/worker/transport/stalled_worker_retryer.go
+++ atc/worker/transport/stalled_worker_retryer.go
@@ -1,12 +1,22 @@
 package transport
 
-import "github.com/concourse/retryhttp"
+import (
+	"context"
+	"fmt"
+	"github.com/concourse/retryhttp"
+)
 
 type UnreachableWorkerRetryer struct {
 	DelegateRetryer retryhttp.Retryer
 }
 
 func (r *UnreachableWorkerRetryer) IsRetryable(err error) bool {
+
+	if err == context.DeadlineExceeded{
+		fmt.Println("ctx_log: IsRetryable DeadlineExceeded")
+		return false
+	}
+
 	if _, ok := err.(WorkerUnreachableError); ok {
 		return true
 	}
